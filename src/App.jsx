f440f773bc010e360a298853503c29e06841203a
Nolan Knill <nolanknill@gmail.com>
2023-11-30 11:34:47 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h1>Browser Storage</h1>
+        <section>
+          <h2>Local Storage Count 📦</h2>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
